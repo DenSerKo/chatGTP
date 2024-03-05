487e502f1609879c52b5b75ce3337aca0acd9079
DenSerKo <89607400164denis@gmail.com>
2024-03-05 20:27:51 +0300

    test

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from g4f.client import Client
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    client = Client()
+    response = client.chat.completions.create(
+        provider="You",
+        model="gpt-4",
+        messages=[{"role": "user", "content": "Привет, расскажи анекдот."}]
+    )
+    return {"message": response.choices[0].message.content}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
